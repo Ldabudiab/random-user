ddcd65d68a1cea29612a2f88aa781bd767c35dba
ldabudiab <ldabudiab@gmail.com>
2021-09-07 11:19:28 -0500

    added user info

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
 import { getUsers } from "./datamanager.js";
+import { users, userList } from "./userdetail.js";
 
 
+const showUsers = () => {
+
+    const postElement = document.querySelector(".user");
+    getUsers().then((allResults) => {
+		postElement.innerHTML = userList(allResults.results);
+	})
+}
 const showUser = () =>{
     getUsers().then(response => console.log(response))
     
@@ -8,3 +16,4 @@ const showUser = () =>{
 }
 
 showUser();
+showUsers();
--- scripts/userdetail.js
+++ scripts/userdetail.js
@@ -1,13 +1,24 @@
 
 
-// export const users = (userObject) => {
+export const users = (resultsObject) => {
 
-//     return `
-//       <section class="user">
-//         <header>          
-//             <h2 class="user__title">${userObject.title}</h2>
-//         </header>
-//         <img class="user__image" src="${userObject.imageURL}" />
-//       </section>
-//     `
-//   }
+    return `
+      <section class="user">
+        <header>          
+            <h2 class="user__name">${resultsObject.name.first}</h2>
+        </header>
+        <img class="user__image" src="${resultsObject.picture.medium}" />
+        <h3 class="user_date">${resultsObject.registered.date}</h3>
+      </section>
+    `
+  }
+
+  export const userList = (allResults) => {
+    let postHTML = "";
+     
+    for (const resultsObject of allResults) {
+        postHTML += users(resultsObject)
+    }
+    return postHTML;
+
+  }
